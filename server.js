c58076eea52f06434b5b0c1e2ae5b20df639c721
Ali Kendir <alikendir2002@gmail.com>
2024-02-11 20:17:44 +0300

    get and post setup for student table contents

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
 const express = require("express");
-const { request } = require("http");
+
+const tableController = require("./tableController");
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
+
+tableController(app);
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-let tableContents = [
-  ["Ad", "Soyad", "T.C. Kimlik Numarasi", " Öğrenci Numarası"],
-  ["Ali", "Kendir", "10000000146", "123456"],
-  ["Alp", "Kendir", "10000000146", "123456"],
-  ["Alp", "Kendir", "10000000146", "123456"],
-];
-app.listen(PORT, () => console.log(`Running Express Server on Port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
 
 app.get("/tableContents", (req, res) => {
   res.send(tableContents);
 });
-
-// app.post("/tableContents", (req, res) => {
-//   tableContents.push(req.body);
-//   res.sendStatus(201);
-// });
