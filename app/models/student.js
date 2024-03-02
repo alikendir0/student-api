5217799fd83e9d8e96457ffa3293996974c08f57
Ali Kendir <alikendir2002@gmail.com>
2024-03-02 05:31:28 +0300

    completed the student services

--- app/models/student.js
+++ app/models/student.js
@@ -1,123 +1,154 @@
-const {ValidationError} = require("./error")
-const Base = require("./base")
+const { ValidationError } = require("./error");
+const Base = require("./base");
 
 class Student extends Base {
-    constructor(ad, soyad, tcNo, ogrenciNo, dersler = []) {
-        super()
-        this.ad = ad
-        this.soyad = soyad
-        this.tcNo = tcNo
-        this.ogrenciNo = ogrenciNo
-        this.dersler = dersler
+  constructor(ad, soyad, tcNo, ogrenciNo, dersler = []) {
+    super();
+    this.ad = ad;
+    this.soyad = soyad;
+    this.tcNo = tcNo;
+    this.ogrenciNo = ogrenciNo;
+    this.dersler = dersler;
+  }
+
+  static create(studentData) {
+    const validation = this.validate(studentData);
+    if (validation === true) {
+      return new Student(
+        studentData.ad,
+        studentData.soyad,
+        studentData.tcNo,
+        studentData.ogrenciNo,
+        studentData.dersler
+      );
+    }
+    return validation;
+  }
+
+  assignCourse(kod) {
+    if (typeof kod === "string" && !this.dersler.includes(kod)) {
+      this.dersler.push(kod);
+      return 0;
+    } else if (this.dersler.includes(kod)) {
+      return 1;
+    } else {
+      return 2;
+    }
+  }
+
+  resetCourses() {
+    this.dersler = [];
+  }
+
+  set(student) {
+    if (data.ad) {
+      this.ad = data.ad;
+    } else if (data.soyad) {
+      this.soyad = data.soyad;
+    } else if (data.tcNo) {
+      this.tcNo = data.tcNo;
+    } else if (data.ogrenciNo) {
+      this.ogrenciNo = data.ogrenciNo;
+    } else if (data.tcNo) {
+      this.tcNo = data.tcNo;
+    }
+    return;
+  }
+
+  static validate(data) {
+    const errors = [];
+
+    if (data.ad === null || data.ad === "" || !(typeof data.ad === "string")) {
+      errors.push(new ValidationError("Name is not valid or empty", data.ad));
     }
 
-    static create(studentData) {
-        const validation = this.validate(studentData)
-        if (validation === true) {
-            return new Student(studentData.ad, studentData.soyad, studentData.tcNo, studentData.ogrenciNo, studentData.dersler)
-        }
-        return validation
+    if (
+      data.soyad === null ||
+      data.soyad === "" ||
+      !(typeof data.soyad === "string")
+    ) {
+      errors.push(
+        new ValidationError("Surname is not valid or empty", data.soyad)
+      );
     }
 
-    assignCourse(kod) {
-        if (typeof kod === "string" && !this.dersler.includes(kod)) {
-            this.dersler.push(kod)
-            return 0
-        } else if (this.dersler.includes(kod)) {
-            return 1
-        } else {
-            return 2
-        }
+    if (!this.tcNoCheck(data.tcNo)) {
+      errors.push(new ValidationError("TC number is not valid", data.tcNo));
     }
 
-    resetCourses() {
-        this.dersler = []
+    if (data.ogrenciNo && data.ogrenciNo.length !== 6) {
+      errors.push(
+        new ValidationError("Student number is not valid", data.ogrenciNo)
+      );
+    } else if (data.ogrenciNo === null || data.ogrenciNo === "") {
+      errors.push(
+        new ValidationError("Student number can't be empty", data.ogrenciNo)
+      );
     }
 
-    static validate(data) {
-        const errors = []
-
-        if (data.ad === null || data.ad === "" || !(typeof data.ad === "string")) {
-            errors.push(new ValidationError("Name is not valid or empty", data.ad))
-        }
-
-        if (data.soyad === null || data.soyad === "" || !(typeof data.soyad === "string")) {
-            errors.push(new ValidationError("Surname is not valid or empty", data.soyad))
-        }
-
-        if (!this.tcNoCheck(data.tcNo)) {
-            errors.push(new ValidationError("TC number is not valid", data.tcNo))
-        }
-
-        if (data.ogrenciNo && data.ogrenciNo.length !== 6) {
-            errors.push(new ValidationError("Student number is not valid", data.ogrenciNo))
-        } else if (data.ogrenciNo === null || data.ogrenciNo === "") {
-            errors.push(new ValidationError("Student number can't be empty", data.ogrenciNo))
-        }
-
-        if (data.dersler && data.dersler instanceof Array) {
-            errors.push(new ValidationError("Courses are not valid", data.dersler))
-        }
-
-        if (errors.length > 0) {
-            return errors
-        }
-
-        return true
+    if (data.dersler && !data.dersler instanceof Array) {
+      errors.push(new ValidationError("Courses are not valid", data.dersler));
     }
 
-    static tcNoCheck(tcNo) {
-        var temp = String(tcNo).split("").map(Number)
-        if (!/^\d{11}$/.test(tcNo)) return false
-
-        let temp10 = 0
-        let temp11 = 0
-        for (let i = 0; i < temp.length; i++) {
-            switch (i) {
-                case 0:
-                    if (temp[i] == 0) {
-                        return false
-                    }
-                    temp10 += temp[i]
-                    break
-                case 1:
-                    temp11 += temp[i]
-                    break
-                case 2:
-                    temp10 += temp[i]
-                    break
-                case 3:
-                    temp11 += temp[i]
-                    break
-                case 4:
-                    temp10 += temp[i]
-                    break
-                case 5:
-                    temp11 += temp[i]
-                    break
-                case 6:
-                    temp10 += temp[i]
-                    break
-                case 7:
-                    temp11 += temp[i]
-                    break
-                case 8:
-                    temp10 += temp[i]
-                    break
-                case 9:
-                    if (!((temp10 * 7 - temp11) % 10 == temp[i])) {
-                        return false
-                    }
-                    temp11 += temp[i]
-                    break
-                case 10:
-                    if (!((temp10 + temp11) % 10 == temp[i])) {
-                        return false
-                    }
-                    break
-            }
-        }
-        return true
+    if (errors.length > 0) {
+      return errors;
     }
+
+    return true;
+  }
+
+  static tcNoCheck(tcNo) {
+    var temp = String(tcNo).split("").map(Number);
+    if (!/^\d{11}$/.test(tcNo)) return false;
+
+    let temp10 = 0;
+    let temp11 = 0;
+    for (let i = 0; i < temp.length; i++) {
+      switch (i) {
+        case 0:
+          if (temp[i] == 0) {
+            return false;
+          }
+          temp10 += temp[i];
+          break;
+        case 1:
+          temp11 += temp[i];
+          break;
+        case 2:
+          temp10 += temp[i];
+          break;
+        case 3:
+          temp11 += temp[i];
+          break;
+        case 4:
+          temp10 += temp[i];
+          break;
+        case 5:
+          temp11 += temp[i];
+          break;
+        case 6:
+          temp10 += temp[i];
+          break;
+        case 7:
+          temp11 += temp[i];
+          break;
+        case 8:
+          temp10 += temp[i];
+          break;
+        case 9:
+          if (!((temp10 * 7 - temp11) % 10 == temp[i])) {
+            return false;
+          }
+          temp11 += temp[i];
+          break;
+        case 10:
+          if (!((temp10 + temp11) % 10 == temp[i])) {
+            return false;
+          }
+          break;
+      }
+    }
+    return true;
+  }
 }
-module.exports = Student
+module.exports = Student;
--- app/services/student.js
+++ app/services/student.js
@@ -1,60 +1,115 @@
-const fs = require("fs")
-const Student = require("../models/student")
-const fileManager = require("../managers/file")
-const {Response, ResponseStatus} = require("../models/response")
+const Student = require("../models/student");
+const fileManager = require("../managers/file");
+const { Response, ResponseStatus } = require("../models/response");
 
-const studentsPath = "data/students.json"
+const studentsPath = "data/students.json";
 
 const list = () => {
-    const students = fileManager.getFile(studentsPath)
-    return new Response(ResponseStatus.SUCCESS, students)
-}
+  const students = fileManager.getFile(studentsPath);
+  return new Response(ResponseStatus.SUCCESS, students);
+};
 
-const get = (id) => {
-    const students = fileManager.getFile(studentsPath)
+const getCourses = (id) => {
+  const students = fileManager.getFile(studentsPath);
 
-    const student = students.filter((student) => student.id === Number(id))[0]
+  const student = students.filter((student) => student.id === Number(id))[0];
 
-    if (student) {
-        return new Response(ResponseStatus.SUCCESS, student)
-    } else {
-        return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found")
-    }
-}
+  if (student) {
+    return new Response(ResponseStatus.SUCCESS, student.dersler);
+  } else {
+    return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found");
+  }
+};
 
 const del = (id) => {
-    const students = fileManager.getFile(studentsPath)
+  const students = fileManager.getFile(studentsPath);
 
-    const index = students.findIndex((student) => student.id === id)
-
-    if (index > -1) {
-        students.splice(index, 1)
-        fileManager.saveFile(studentsPath, students)
-
-        return new Response(ResponseStatus.SUCCESS, null)
-    } else {
-        return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found")
+  const index = students.findIndex((student) => {
+    if (student.id === Number(id)) {
+      return id;
     }
-}
+  });
+
+  if (index > -1) {
+    students.splice(index, 1);
+    fileManager.saveFile(studentsPath, students);
+
+    return new Response(ResponseStatus.SUCCESS, null);
+  } else {
+    return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found");
+  }
+};
 
 const save = (data) => {
-    const students = fileManager.getFile(studentsPath)
+  const students = fileManager.getFile(studentsPath);
 
-    const student = Student.create(data)
+  const student = Student.create(data);
 
-    if (student instanceof Student) {
-        students.push(student)
-        fileManager.saveFile(studentsPath, students)
+  if (student instanceof Student) {
+    students.push(student);
+    fileManager.saveFile(studentsPath, students);
 
-        return new Response(ResponseStatus.CREATED, student)
+    return new Response(ResponseStatus.CREATED, student);
+  } else {
+    return new Response(
+      ResponseStatus.BAD_REQUEST,
+      student,
+      "Invalid student data"
+    );
+  }
+};
+
+const reset = (ids) => {
+  var success = [];
+  var unsuccess = [];
+  const students = fileManager.getFile(studentsPath);
+
+  for (const i in ids) {
+    const id = ids[i];
+    console.log(id);
+    const index = students.findIndex((student) => student.id === id);
+    if (index != -1) {
+      students[index].dersler = [];
+      success.push(id);
     } else {
-        return new Response(ResponseStatus.BAD_REQUEST, student, "Invalid student data")
+      unsuccess.push(id);
     }
-}
+  }
+  fileManager.saveFile(studentsPath, students);
+  if (success.length > 0) {
+    return new Response(
+      ResponseStatus.SUCCESS,
+      `Successful ids:[${success}] Unsuccessful ids:[${unsuccess}]`
+    );
+  } else {
+    return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found");
+  }
+};
+
+const assign = (id, courses) => {
+  const students = fileManager.getFile(studentsPath);
+  const index = students.findIndex((student) => student.id === Number(id));
+  var temp = 0;
+  if (index != -1) {
+    for (const c in courses) {
+      if (!students[index].dersler.includes(courses[c])) {
+        students[index].dersler.push(courses[c]);
+        temp++;
+      }
+    }
+    if (temp > 0) {
+      fileManager.saveFile(studentsPath, students);
+      return new Response(ResponseStatus.SUCCESS, `Success!`);
+    } else return new Response(ResponseStatus.CONFLICT, `Code Conflict!`);
+  } else
+    return new Response(ResponseStatus.BAD_REQUEST, null, "Student not found");
+};
 
 module.exports = {
-    list,
-    get,
-    del,
-    save
-}
+  list,
+  getCourses,
+  del,
+  save,
+  reset,
+  assign,
+};
